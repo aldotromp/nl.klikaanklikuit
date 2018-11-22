fc56f76d2325301c7f5f4b49fdc2f54d73270b8f
Kyle Taylor Parkins <kyle@athom.com>
2018-11-22 14:49:37 +0100

    Tried to fix debug logging

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 
 const Homey = require('homey');
 
-class MyApp extends Homey.App {
+class KlikAanKlikUit extends Homey.App {
 
 	onInit() {
 
-		this.log('MyApp is running...');
+		this.log('KlikAanKlikUit is running...');
 
 	}
 
 }
 
-module.exports = MyApp;
+module.exports = KlikAanKlikUit;
